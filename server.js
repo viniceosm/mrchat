2f894dbc9aec71a63f1cd0e9fd3cbbbaee0901b9
viniceosm <miiller.vini@gmail.com>
2017-01-25 23:43:16 -0200

    primeiro commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+users = [];
+connections = [];
+
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
+app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+
+server.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname +'/index.html');
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('user disconnected');
+	});
+});
+
+io.on('connection', function(socket){
+	socket.on('chat message', function(msg){
+		io.emit('chat message', msg);
+ 	});
+
+	socket.on('is typing', function(data){
+		socket.broadcast.emit('typing', data);
+	});
+});
