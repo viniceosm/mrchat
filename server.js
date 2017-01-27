b67ecb39f3d9127e392650c9d4d16cd19aae8336
viniceosm <miiller.vini@gmail.com>
2017-01-27 18:29:44 -0200

    cliente

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ connections = [];
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.use(Express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT, function () {
 	console.log('listening on *:'+process.env.PORT);
