458e2a648ffaacbda2d9fc58f98ff59642cc8146
viniceosm <miiller.vini@gmail.com>
2017-01-29 14:53:05 -0200

    game

---
+++ public/js/chat.js
@@ -0,0 +1,63 @@
+var imgTabuleiro = new Image;
+var imgPersonagem = new Image;
+imgTabuleiro.src = '../img/tabuleiro.png';
+imgPersonagem.src = '../img/personagem.png';
+
+window.onload = function() {
+	var socket = io.connect();
+	$('#formUsuario').submit(function(e){
+		e.preventDefault();
+		socket.emit('novo usuario', $('#nomeUsuario').val(), function(dado){
+			if(dado){
+				$('#formUsuario').hide();
+				$('#areaMensagem').show();
+			}
+		});
+		$('#nomeUsuario').val('');
+	});
+
+	$('#paneMensagem').submit(function(){
+		socket.emit('chat message', $('#txtMensagem').val());
+		$('#txtMensagem').val('');
+		return false;
+	});
+	socket.on('chat message', function(dado){
+		$('#mensagens').append('<li><strong>'+dado.user +'</strong>:'+ dado.msg);
+	});
+
+	$(window).keyup(function (e)  {
+		if(e.keyCode == 39){
+			socket.emit('mover personagem', "direita");
+		}else if(e.keyCode == 37){
+			socket.emit('mover personagem', "esquerda");
+		}else if(e.keyCode == 38){
+			socket.emit('mover personagem', "cima");
+		}else if(e.keyCode == 40){
+			socket.emit('mover personagem', "baixo");
+		}else if(e.keyCode == 13){
+			$('#txtMensagem').focus();
+		}
+	});
+
+	canvas = document.getElementById('tabuleiro');
+	ctx = canvas.getContext("2d");
+
+	canvas.height= 468;
+	canvas.width= 1049;
+
+	ctx.drawImage( imgTabuleiro, 0, 0);
+
+	socket.on('mostrar personagens', function(dados){
+		ctx.clearRect(0,0,canvas.width,canvas.height);
+		ctx.drawImage( imgTabuleiro, 0, 0);
+
+		for(var i=0;i<dados.length;i++){
+			ctx.drawImage( imgPersonagem, 0, 0, 45, 110,dados[i].x, dados[i].y, 45, 110);
+			ctx.font = "25px Tahoma";
+			ctx.fillStyle = "white";
+			ctx.strokeStyle = "black";
+			ctx.fillText(dados[i].username, dados[i].x, dados[i].y);
+			ctx.strokeText(dados[i].username, dados[i].x, dados[i].y);
+		}
+	});
+};
--- server.js
+++ server.js
@@ -1,35 +1,66 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 users = [];
 connections = [];
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+// app.use('/js', express.static(__dirname + '/node_modules/socket.io-client/dist'));
 
-server.listen(3000, function(){
-	console.log('listening on *:3000');
-});
+server.listen(process.env.port || 3000);
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname +'/index.html');
 });
 
 io.on('connection', function(socket){
-	console.log('a user connected');
-	socket.on('disconnect', function(){
-		console.log('user disconnected');
-	});
-});
+	console.log('Um usuario conectado');
+	connections.push(socket);
 
-io.on('connection', function(socket){
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+	socket.on('disconnect', function(){
+		users.splice(users.map(function(e) { return e.username; }).indexOf(socket.username), 1);
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('Um usuario disconnectado');
+		io.sockets.emit('mostrar personagens', users);
+	});
+
+	socket.on('chat message', function(dado){
+		io.emit('chat message', {msg: dado, user: socket.username});
  	});
 
-	socket.on('is typing', function(data){
-		socket.broadcast.emit('typing', data);
+	socket.on('is typing', function(dado){
+		socket.broadcast.emit('typing', dado);
 	});
+
+	socket.on('novo usuario', function(dado, callback){
+		callback(true);
+		socket.id = users.length;
+		socket.username = dado;
+		socket.x = 10;
+		socket.y = 10;
+		users.push({username: socket.username, x:socket.x, y:socket.y});
+
+		io.sockets.emit('mostrar personagens', users);
+	});
+
+	socket.on('mover personagem', function(direcao){
+		if(direcao=="direita"){
+			socket.x+=30;
+		}else if (direcao=="esquerda") {
+			socket.x-=30;
+		}else if (direcao=="cima") {
+			socket.y-=30;
+		}else if (direcao=="baixo") {
+			socket.y+=30;
+		}
+		users[socket.id].x = socket.x;
+		users[socket.id].y = socket.y;
+		io.sockets.emit('mostrar personagens', users);
+	});
+
 });
