4386a146e30d01c5c65d27eaac4592e62f823dd6
viniceosm <miiller.vini@gmail.com>
2017-01-29 15:43:51 -0200

    comentando tudo nessa porra

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
-app.use('/js', express.static(__dirname + '/node_modules/socket.io-client/dist'));
+// app.use('/js', express.static(__dirname + '/node_modules/socket.io-client/dist'));
 
 server.listen(process.env.port || 5000);
 
